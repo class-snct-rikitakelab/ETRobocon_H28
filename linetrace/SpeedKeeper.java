5245682e2a232a62e11d172036c69d34dcb41068
YutaroKikuchi <s1000614@sendai-nct.jp>
2016-07-26 17:05:56 +0900

    Forward値を区間制御する機能を組み込んだ

--- linetrace/SpeedKeeper.java
+++ linetrace/SpeedKeeper.java
@@ -4,8 +4,14 @@ public class SpeedKeeper {
 
 	private static float spTarget;
 
+	private static float forwardTargets[] = {100.0F,30.0F,100.0F,30.0F,100.0F,30.0F,100.0F};
+
 	public void setTarget(float target) {
 		spTarget = target;
+	}
+
+	public float getForwardTarget(int index){
+		return forwardTargets[index];
 	}
 
 	public float getTarget() {
