d81ad9f79c9b008cba2261860b1f898444f3f7bf
AbeYusaku <s1100603@sendai-nct.jp>
2016-09-01 12:19:24 +0900

    TestLineTrace.javaをRcource.javaに適用

--- linetrace/Rcourse.java
+++ linetrace/Rcourse.java
@@ -1,52 +1,74 @@
 package linetrace;
 
+import hardware.Hardware;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
-import drive_control.BrightMeasure;
-import drive_control.BrightTargetKeeper;
-import hardware.Hardware;
-import lejos.hardware.Sound;
 import lejos.hardware.lcd.LCD;
 import lejos.utility.Delay;
 import starter.Starter;
+import steps.StepSolver;
+import area_param.DistanceMeasure;
+import drive_control.BrightMeasure;
+import drive_control.BrightTargetKeeper;
 
 public class Rcourse {
+
+	public static int START_COMMAND = 71;
+
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
 
 		final LineTracer lt = new LineTracer();
 		Starter start = new Starter();
+		final DistanceMeasure dm = new DistanceMeasure();
+		StepSolver step = new StepSolver();
+		float distance = 0.0F;
 
 		calibration();
+		BrightTargetKeeper tk = new BrightTargetKeeper();
+		step.blackColor = tk.getBlack();
+		step.whiteColor = tk.getWhite();
+		step.grayColor  = tk.getGray();
 
 		start.start();
 
-		Sound.beep();
+		//Sound.beep();
 
+
+		//↓のタイマは輝度値制御でライントレースする。周期は4ms
 		Timer driveTimer = new Timer();
 		TimerTask driveTask = new TimerTask(){
 
 			public void run(){
-
 				lt.linetrace();
-
 			}
 		};
 
+		//下のタイマは走行距離測定して，PID係数と速度を切り替えている。周期は100ms
+		Timer distanceTimer = new Timer();
+		DistanceTask distancetask = new DistanceTask(lt,dm,distance);
+
+
 		driveTimer.scheduleAtFixedRate(driveTask, 0, 4);
+		distanceTimer.scheduleAtFixedRate(distancetask, 0, 100);
 
+		//一定距離走るとループから抜ける
 		while(true){
+			distance = distancetask.getDistance();
 
-			if(Hardware.touchSensorIsPressed() == true){
+			if(distance > 8.0F){
+				driveTimer.cancel();
+				distanceTimer.cancel();
+				LCD.drawString("kaidan", 1, 1);
+				step.solveStep();
 				break;
 			}
 
 			Delay.msDelay(20);
 		}
-
-
 
 	}
 
@@ -114,5 +136,6 @@ public class Rcourse {
 		tk.setStep(bright.measureBrightness());
 		LCD.clear();
 	}
+
 
 }
