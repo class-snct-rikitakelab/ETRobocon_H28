06fca0d5a9008c3e32be7de71b37e1e385f84ffb
OTM-07F <s1100608@sendai-nct.jp>
2016-09-04 19:37:12 +0900

    0除算も考慮

--- drive_control/BrightMeasure.java
+++ drive_control/BrightMeasure.java
@@ -12,6 +12,11 @@ public class BrightMeasure {
 		//引数:白および黒の輝度値基準
 		float brightness;
 		brightness = Hardware.getBrightness();
-		return (White - brightness)/(White - Black);	//白を1、黒を0として正規化
+		if(White != Black){
+			return (White - brightness)/(White - Black);	//白を1、黒を0として正規化
+		}
+		else{
+			return 0.5F;//０除算になる場合を除外。ライン上にいるものとする
+		}
 	}
 }
